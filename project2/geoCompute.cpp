d1ad24f2b19e2b651a4151ea215965e27e50694d
astrange <astrange>
2011-03-03 01:39:50 +0000

    fix typo errors in geoCompute using clang

--- project2/geoCompute.cpp
+++ project2/geoCompute.cpp
@@ -4,68 +4,73 @@
 
 class Point
 {
-    public:
-        Point(int x, int y): m_x(x), m_y(y) { }
-        virtual ~Point() { }
-        
-        int getX() { return m_x; }
-        int getY() { return m_y; }
-        
-        int setX(int x) { m_x = x; }
-        int setY(int y) { m_y = y; }
-        
-        virtual double distance(Point* p)
-        {
-            assert(NULL != p);
-            int dx = m_x - p.m_x;
-            int dy = m_y - p.m_y;
-            return sqrt(dx*dx + dy*dy);
-        }
-        
-    private:
-        int m_x;
-        int m_y;
+public:
+    Point(int x, int y): m_x(x), m_y(y) { }
+    Point() : m_x(0), m_y(0) {}
+    virtual ~Point() { }
+    
+    int getX() { return m_x; }
+    int getY() { return m_y; }
+    
+    int setX(int x) { return m_x = x; }
+    int setY(int y) { return m_y = y; }
+    
+    virtual double distance(Point* p)
+    {
+        assert(p);
+        int dx = m_x - p->m_x;
+        int dy = m_y - p->m_y;
+        return sqrt(dx*dx + dy*dy);
+    }
+    
+private:
+    int m_x;
+    int m_y;
 };
 
-class LemonPoint : Point
+struct LemonPoint : Point
 {
-    LemonPoint(int x, int y): Point(x, y) { }
-    virtual ~LemonPoint()                 { }
+    LemonPoint(int x, int y): Point(x, y) {}
+    virtual ~LemonPoint()                 {}
 };
 
-class RobotPoint : Point
+struct RobotPoint : Point
 {
-    public Robot(int x, int y): Point(x, y) { }
-    virtual ~Robot()                        { }
+    RobotPoint(int x, int y): Point(x, y) {}
+    virtual ~RobotPoint()                 {}
+};
+
+enum Direction {
+    left,
+    center,
+    right
 };
 
 class MathGod
 {
-    public:
+public:
     
-        MathGod(RobotPoint* R LemonPoint* L):
-        {
-            assert(NULL != R);
-            assert(NULL != L);
-            m_intersect     = new Point(L->getX(),R->getY());
-            
-            m_hypotenuse    = R->distance(L);
-            m_parallelLine  = m_intersect->distance(R);
-            m_othogonalLine = m_intersect->distance(L);
-            
-            m_angle         = asin(m_parallelLine/m_hypotenuse);
-            
-            if(R.getX()> L.getX())
-                m_side = left;
-            
-            else if (R.getX() == L.getX()
-                m_side = center;
-            
-            else
-                m_side = right;
-            
-        }
-
+    MathGod(RobotPoint* R, LemonPoint* L)
+    {
+        assert(NULL != R);
+        assert(NULL != L);
+        m_intersect     = Point(L->getX(),R->getY());
+        
+        m_hypotenuse    = R->distance(L);
+        m_parallelLine  = m_intersect.distance(R);
+        m_orthogonalLine = m_intersect.distance(L);
+        
+        m_angle         = asin(m_parallelLine/m_hypotenuse);
+        
+        if(R->getX() > L->getX())
+            m_side = left;
+        else if (R->getX() == L->getX()) // <--- this should be fuzzy
+            m_side = center;
+        else
+            m_side = right;
+        
+    }
+    
     double GetAngle()          const { return m_angle;          }
     double GetDirection()      const { return m_side;           }
     Point  GetIntersect()      const { return m_intersect;      }
@@ -78,37 +83,28 @@ class MathGod
     void   SetHypotenuse     (double hypotenuse)     { m_hypotenuse     = hypotenuse;     }
     void   SetParallelLine   (double parallelLine)   { m_parallelLine   = parallelLine;   }
     void   SetOrthogonalLine (double orthogonalLine) { m_orthogonalLine = orthogonalLine; }
- 
-
-    private:
-        double m_hypotenuse;
-        double m_parallelLine;
-        double m_orthogonalLine;
-        double m_angle;
-        Point  m_intersect;
-        Direction m_side;
+    
+    
+private:
+    double m_hypotenuse;
+    double m_parallelLine;
+    double m_orthogonalLine;
+    double m_angle;
+    Point  m_intersect;
+    Direction m_side;
 };
-
-
-enum Direction
-{
-    left =0,
-    center =1,
-    right = 2,
-};
-
 
 class Decision
 {
-    public:
-        //this will need to be the math function eventually
-        Decision(double angle, double pLine, Direction side):
-        {
-            m_side  = side;
-            m_angle = angle;
-            m_pLine = pLine;
-        }
-        
+public:
+    //this will need to be the math function eventually
+    Decision(double angle, double pLine, Direction side)
+    {
+        m_side  = side;
+        m_angle = angle;
+        m_pLine = pLine;
+    }
+    
     void giveOrders() 
     {
         CURL *curl;
@@ -123,14 +119,13 @@ class Decision
         
         if(curl) 
         {
-            if (side == left)
+            if (m_side == left)
             {
                 rovio_turnLeftByDegree(curl,counter);
                 // add a rovio forward comand here
             }
-            else if (side == center)
-                rovio_forward(curl,((int) p_line),5);
-        
+            else if (m_side == center)
+                rovio_forward(curl,m_pLine,5);
             else
             {
                 rovio_turnRightByDegree(curl,counter); 
@@ -138,9 +133,9 @@ class Decision
             }    
         }
     }
-    private:
-        double m_angle;
-        double m_pLine;
-        Direction m_side;
-
+private:
+    double m_angle;
+    double m_pLine;
+    Direction m_side;
+    
 };
