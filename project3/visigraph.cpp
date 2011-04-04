46509b4da9496c7ce2231007620849ae59473a9e
astrange <astrange>
2011-04-04 05:17:22 +0000

    update, prints path to follow now

--- project3/visigraph.cpp
+++ project3/visigraph.cpp
@@ -25,8 +25,66 @@ CvMemStorage *gStorage = NULL;
 static std::vector<CvBox2D> obstacleboxes;
 static VisiLibity::Environment *visibility;
 
+static std::vector<VisiLibity::Point> following_path;
+
+#pragma mark -- camera control goes here
+
+static IplImage *fetch_obstacle_image()
+{
+    CvCapture *filecapture = cvCaptureFromFile("robot_image_obstacles.png");
+    IplImage *obstacles;
+    
+    cvGrabFrame(filecapture);
+    obstacles = cvCloneImage(cvRetrieveFrame(filecapture, 0));
+    cvReleaseCapture(&filecapture);
+    return obstacles;
+}
+
 static int robot_x = 360, robot_y = 235;
+static float robot_angle = M_PI / 2; // straight up
 static int goal_x  = 685, goal_y  = 225;
+
+static float angle_towards(int x1, int y1, int x2, int y2)
+{
+    int xv = x2 - x1;
+    int yv = y2 - y1;
+    
+    //printf("xv %d yv %d\n", xv, yv);
+    
+    float res = atan2f(yv, xv);
+    
+    res = (2*M_PI) - res;
+    res = fmodf(res, 2*M_PI);
+    return res;
+}
+
+static void drive_to_point(VisiLibity::Point p)
+{
+    int cur_x = robot_x, cur_y = robot_y;
+    int next_x = p.x(), next_y = p.y();
+    float angle = angle_towards(cur_x, cur_y, next_x, next_y);
+    
+    printf("drive from %d,%d to %d,%d\n", robot_x, robot_y, next_x, next_y);
+    printf("change orientation from %f (%f) to %f (%f)\n", robot_angle, robot_angle * (180./M_PI)
+                                                         , angle, angle * (180./M_PI));
+    
+    robot_x = next_x;
+    robot_y = next_y;
+    robot_angle = angle;
+}
+
+// drives along following_path
+static void drive_to_goal()
+{
+    using namespace VisiLibity;
+    for (int i = 1; i < following_path.size(); i++) {
+        Point p = following_path[i];
+        
+        drive_to_point(p);
+    }
+}
+
+#pragma mark -- camera control ends here
 
 static IplImage *same_size_image_8bit(IplImage *frame)
 {
@@ -74,19 +132,13 @@ static void draw_point(IplImage *frame, int x, int y, int color)
 // with the visibility graph drawn on top
 static IplImage *get_obstacle_image()
 {
-    CvCapture *filecapture = cvCaptureFromFile("robot_image_obstacles.png");
-    IplImage *obstacles;
-    
-    cvGrabFrame(filecapture);
-    obstacles = cvCloneImage(cvRetrieveFrame(filecapture, 0));
-    cvReleaseCapture(&filecapture);
-    
+    IplImage *obstacles = fetch_obstacle_image();
     IplImage *grey = same_size_image_8bit(obstacles);
     
     cvCvtColor(obstacles, grey, CV_BGR2GRAY);
     
     CBlobResult blobs = CBlobResult(grey, NULL, 0, true);
-    //blobs.Filter(blobs, B_INCLUDE, CBlobGetMean(), B_GREATER, 10);
+    blobs.Filter(blobs, B_INCLUDE, CBlobGetMean(), B_GREATER, 10);
     blobs.Filter(blobs, B_INCLUDE, CBlobGetArea(), B_GREATER, 20);
     blobs.Filter(blobs, B_INCLUDE, CBlobGetArea(), B_LESS, 250000);
 
@@ -99,8 +151,8 @@ static IplImage *get_obstacle_image()
         CvPoint2D32f pt[4];
         
         blob.FillBlob(obstacles, CV_RGB(0,255,0));
-        printf("blob %d has %d edges\n", i, blob.edges->total);
-        printf("%d - x %f to %f, y %f to %f\n", i, blob.MinX(), blob.MaxX(), blob.MinY(), blob.MaxY());
+        //printf("blob %d has %d edges\n", i, blob.edges->total);
+        //printf("%d - x %f to %f, y %f to %f\n", i, blob.MinX(), blob.MaxX(), blob.MinY(), blob.MaxY());
         
         {
             CvRect fr;
@@ -132,24 +184,41 @@ static IplImage *get_obstacle_image()
     return obstacles;
 }
 
-static void add_rect(int x, int y)
+static VisiLibity::Point move_point_away(VisiLibity::Point p)
 {
-    using namespace VisiLibity;
-
-    Point pp[4];
-    Point p1(x-1, y-1), p2(x+1, y+1);
+    // find the obstacle whose corner this point is on
+    // then move the point away from its center
+    float cx=0, cy=0;
     
-    pp[0] = p1;
-    pp[1] = p1;
-    pp[2] = p2;
-    pp[3] = p2;
+    for (int i = 0; i < obstacleboxes.size(); i++) {
+        CvBox2D box = obstacleboxes[i];
+        CvPoint2D32f pt[4];
+        
+        cvBoxPoints(box, pt);
+        
+        for (int j = 0; j < 4; j++) {
+            float px = pt[j].x, py = pt[j].y;
+            
+            if (abs(px - p.x()) < 2. && abs(py - p.y()) < 2.) {
+                cx = box.center.x;
+                cy = box.center.y;
+                goto done;
+            }
+        }
+    }
     
-    pp[1].set_x(p2.x());
-    pp[3].set_x(p1.x());
+    return p;
     
-    std::vector<Point> pv(pp, pp+4);
-    Polygon hole(pv);
-    visibility->add_hole(hole);
+done:
+    float cxv = p.x() - cx, cyv = p.y() - cy;
+    float r = sqrtf(cxv * cxv + cyv * cyv);
+    cxv /= r;
+    cyv /= r;
+    
+    cxv *= 35;
+    cyv *= 35;
+    
+    return VisiLibity::Point(p.x() + cxv, p.y() + cyv);
 }
 
 static IplImage *get_visibility_image(IplImage *obstacle_image)
@@ -191,7 +260,7 @@ static IplImage *get_visibility_image(IplImage *obstacle_image)
     
    // printf("vis valid %d area %f dia %f\n", visibility->is_valid(), visibility->area(), visibility->diameter());
     
-    printf("drawing visibility graph\n");
+    //printf("drawing visibility graph\n");
     
     IplImage *visibility_image = cvCloneImage(obstacle_image);
 
@@ -221,14 +290,14 @@ static IplImage *get_visibility_image(IplImage *obstacle_image)
      
     printf("%d vertices, %d edges in graph\n", nvertices, nlines);
     
-    printf("drawing visibility poly of robot\n");
+    //printf("drawing visibility poly of robot\n");
     
     Point robotp(robot_x, robot_y);
     Point goalp(goal_x, goal_y);
     
     {
         Visibility_Polygon rpoly(robotp, *visibility, 5);
-        printf("vpoly area %f dia %f\n", rpoly.area(), rpoly.diameter());
+        //printf("vpoly area %f dia %f\n", rpoly.area(), rpoly.diameter());
         
         for (int i = 0; i < rpoly.n(); i++) {
             Point p = rpoly[i];
@@ -241,14 +310,21 @@ static IplImage *get_visibility_image(IplImage *obstacle_image)
         }
     }
     
-    printf("drawing shortest path\n");
+    //printf("drawing shortest path\n");
     
     {
         Polyline path = visibility->shortest_path(robotp, goalp, 5);
         
+        following_path.clear();
+        
+        for (int i = 0; i < path.size(); i++) {
+            Point p = move_point_away(path[i]);
+            following_path.push_back(p);
+        }
+        
         for (int i = 0; i < (path.size()-1); i++) {
-            Point p = path[i];
-            Point p1 = path[i+1];
+            Point p = following_path[i];
+            Point p1 = following_path[i+1];
             CvPoint cp1, cp2;
             cp1.x = p.x(); cp1.y = p.y();
             cp2.x = p1.x();cp2.y = p1.y();
@@ -258,7 +334,7 @@ static IplImage *get_visibility_image(IplImage *obstacle_image)
             cvLine(visibility_image, cp1, cp2, CV_RGB(255, 255, 255), 4, 8, 0);
         }
         
-        printf("path size %d length %f\n", path.size(), path.length());
+        //printf("path size %d length %f\n", path.size(), path.length());
     }
     
     return visibility_image;
@@ -276,6 +352,8 @@ int main(int argc, char *argv[])
     visibility_image = get_visibility_image(obstacle_image);
     
     cvShowImage("visibility", visibility_image);
+    
+    drive_to_goal();
     
     while (1) {
         cvWaitKey(2000);
